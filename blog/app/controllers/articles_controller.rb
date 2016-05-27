c5249807b2e6415612b4fac7d67f0fddac4cbf98
Finn Howell <fhowell@onemedical.com>
2016-05-27 09:59:25 -0700

    Done with Flash

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,56 +2,56 @@ class ArticlesController < ApplicationController
 
     http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
 
-	def index
-		@articles = Article.all
-	end
+  def index
+    @articles = Article.all
+  end
 
-	def show
-		@article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-	def new
-		@article = Article.new
-	end
+  def new
+    @article = Article.new
+  end
 
-	def edit
-		@article = Article.find(params[:id])
-	end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-	def create
-		@article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
 
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
-	end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@article = Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
 
-		if @article.update(article_params)
-			redirect_to @article
-		else
-			render 'edit'
-		end
-	end
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@article = Article.find(params[:id])
-		#if article has comment do not destroy, else destroy
-		if @article.comments
-			redirect_to @article
-		else
-			@article.destroy
-		end
+  def destroy
+    @article = Article.find(params[:id])
+    #if article has comment do not destroy, else destroy
+    if @article.comments.length > 0
+      redirect_to articles_path, flash: { some_key: "You can't destroy an article with comments." }
+    else
+      @article.destroy
+      redirect_to articles_path
+    end
 
-		#redirect_to articles_path
-	end
+  end
 
-	private
-		def article_params
-			params.require(:article).permit(:title, :text)
-		end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
